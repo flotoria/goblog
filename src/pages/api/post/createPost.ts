a9d42e2bff69c862883ad2114b8929630cce4e4b
Ryan <rqn.pham@gmail.com>
2024-05-03 19:23:23 -0700

    error

--- src/pages/api/post/createPost.ts
+++ src/pages/api/post/createPost.ts
@@ -77,6 +77,6 @@ export default async function handler(
     }
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: "Internal server error." });
+    return res.status(500).json({ message: "Internal server error.", error: error });
   }
 }
